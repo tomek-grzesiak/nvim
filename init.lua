28aea9cd42d15582b635b4ceb0b490d376dd387d
tomek-grzesiak <tomek.grzesiak@hotmail.com>
2023-11-19 21:42:01 +0100

    trnsparent bg

--- init.lua
+++ init.lua
@@ -643,3 +643,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, opts)
   end,
 })
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
